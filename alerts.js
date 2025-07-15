bc5012c40a123bececc71621b03356d30679dc61
Harsh Rana <harsh.rana1@student.kpu.ca>
2025-07-15 12:04:56 -0700

    Add files via upload

---
+++ alerts.js
@@ -0,0 +1,27 @@
+function showBookingSuccessAlert() {
+  alert('Reservation submitted successfully! We will contact you within 24 hours to confirm your booking.');
+}
+
+function showMenuSavedAlert() {
+  alert('Menu selection saved! You can now proceed to booking.');
+}
+
+function showChefRequiredAlert() {
+  alert('Please select a chef first!');
+}
+
+function showBookingRedirectAlert() {
+  alert('Please select your menu items before booking.');
+}
+
+function showBookingErrorAlert() {
+  alert('There was an error submitting your reservation. Please try again or contact us directly.');
+}
+
+function showContactSuccessAlert() {
+  alert('Thank you for your message! We will get back to you within 24 hours.');
+}
+
+function showContactErrorAlert() {
+  alert('There was an error sending your message. Please try again or contact us directly.');
+} 
